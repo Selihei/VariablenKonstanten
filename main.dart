ba0c57363c39eea35e35f3990799e1b4291c0198
Selihei <selina.heinrichlp@gmail.com>
2025-06-03 11:25:39 +0200

    Added variables

--- main.dart
+++ main.dart
@@ -1,4 +1,15 @@
 void main() {
-  String gdStr = "Guten";
- 
+String gdStr = "Guten";
+String dayStr = "Tag";
+String worldStr = "Welt";
+int d = 20;
+String timeStr = "Zeit";
+int seven = 7;
+int twelve = 12;
+String d = "!";
+int a = 24;
+String c = "Schöne";
+String morning = "Morgen";
+String s = "Heute";
+String spa = " ";
 }
